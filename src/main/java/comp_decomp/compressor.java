5851839dacfc7d90a9d92638a0b9dd0881d53078
anirudhsom123 <anirudhsom007@gmail.com>
2025-09-11 00:56:10 +0530

    Compressor logic 1

--- src/main/java/comp_decomp/compressor.java
+++ src/main/java/comp_decomp/compressor.java
@@ -1,4 +1,29 @@
 package comp_decomp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
 public class compressor {
+   public static void method(File file) throws IOException {
+       String fileDirectory=file.getParent();
+
+       FileInputStream fis=new FileInputStream(file);
+       FileOutputStream fos=new FileOutputStream(fileDirectory+"/compressedFile.gz");
+       GZIPOutputStream GZIP=new GZIPOutputStream(fos);
+
+       Byte[] Buffer=new Byte[1024];
+       int len;
+
+
+
+
+
+    }
+    public static void main(String[] args) throws IOException {
+       File path=new File("/Users/anirudhsom/Desktop/Testfile");
+       method(path);
+    }
 }
--- src/main/java/comp_decomp/decompressor.java
+++ src/main/java/comp_decomp/decompressor.java
@@ -1,4 +1,16 @@
 package comp_decomp;
 
+import java.io.File;
+
 public class decompressor {
+
+    public static void method(File file){
+
+    }
+
+    public static void main(String[] args){
+        File path=new File("");
+
+        method(path);
+    }
 }
